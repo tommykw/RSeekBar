b2562988147804733bdb2ea32ab3737bd10f0c08
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-11-08 14:55:52 +0900

    Compose range seekbar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # RangeSeekBar
+
+## License
+This application is free software. You can redistribute it and/or modify it
+under the terms of the Apache License 2.0.
+
+* http://www.apache.org/licenses/LICENSE-2.0
---
+++ app/src/main/java/tokyo/tommy_kw/rangeseekbar/RangeSeekBar.java
@@ -0,0 +1,13 @@
+package tokyo.tommy_kw.rangeseekbar;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/**
+ * @author Kenji Tomita (destinys.lotus.1108mn@gmail.com)
+ */
+public class RangeSeekBar<T extends Number> extends ImageView {
+    public RangeSeekBar(Context context) {
+        super(context);
+    }
+}
