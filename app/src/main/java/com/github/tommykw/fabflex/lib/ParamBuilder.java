09009dda0959234f7d9a5f87368b653bc101881e
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-03-31 21:57:22 +0900

    Add interface for ParamBuilder

--- app/src/main/java/com/github/tommykw/fabflex/lib/ParamBuilder.java
+++ app/src/main/java/com/github/tommykw/fabflex/lib/ParamBuilder.java
@@ -1,9 +1,17 @@
 package com.github.tommykw.fabflex.lib;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+
 /**
  * parameter builder interface
  */
 public interface ParamBuilder {
     void add();
-    void remove();
+    FabFlex remove(@IntRange(from = 0) int index);
+    void colorChanged(@ColorInt int color);
+    void iconChanged(@NonNull Drawable drawable);
+    void changedAt(@IntRange(from = 0) int index, @NonNull FabFlex fabFlex);
 }
--- app/src/main/java/com/github/tommykw/fabflex/lib/ParamBuilderImpl.java
+++ app/src/main/java/com/github/tommykw/fabflex/lib/ParamBuilderImpl.java
@@ -1,9 +1,45 @@
 package com.github.tommykw.fabflex.lib;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+
 /**
  * parameter builder class
  */
-public class ParamBuilderImpl {
+public class ParamBuilderImpl implements ParamBuilder, ItemAdapter.OnFabClickListener {
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public FabFlex remove(@IntRange(from = 0) int index) {
+        return null;
+    }
+
+    @Override
+    public void colorChanged(@ColorInt int color) {
+
+    }
+
+    @Override
+    public void iconChanged(@NonNull Drawable drawable) {
+
+    }
+
+    @Override
+    public void changedAt(@IntRange(from = 0) int index, @NonNull FabFlex fabFlex) {
+
+    }
+
+    @Override
+    public void onClick(FabFlex fabFlex, int position) {
+
+    }
+
     public interface OnClickListener {
         void onClick(int position);
     }
