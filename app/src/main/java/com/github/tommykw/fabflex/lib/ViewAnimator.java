516e0d7709350f73760903ebf183aee94c08bdff
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-04-08 23:30:56 +0900

    Add translation

--- app/src/main/java/com/github/tommykw/fabflex/lib/ViewAnimator.java
+++ app/src/main/java/com/github/tommykw/fabflex/lib/ViewAnimator.java
@@ -1,8 +1,12 @@
 package com.github.tommykw.fabflex.lib;
 
 import android.animation.Animator;
+import android.content.Context;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.view.animation.OvershootInterpolator;
 
 import com.github.tommykw.fabflex.R;
 
@@ -10,6 +14,7 @@ import com.github.tommykw.fabflex.R;
  * This implementation of {@link RecyclerView} provides basic animations
  */
 public class ViewAnimator extends DefaultItemAnimator {
+    private static final OvershootInterpolator oi = null;
     @Override
     public boolean animateAdd(RecyclerView.ViewHolder holder) {
         dispatchAddFinished(holder);
@@ -61,7 +66,7 @@ public class ViewAnimator extends DefaultItemAnimator {
         holder.itemView.setAlpha(1);
         holder.itemView.animate()
                 .alpha(0)
-                .translationYBy(0)
+                .translationYBy(getTranslation(holder))
                 .setInterpolator(null)
                 .setListener(new Animator.AnimatorListener() {
                     @Override
@@ -84,5 +89,12 @@ public class ViewAnimator extends DefaultItemAnimator {
 
                     }
                 }).start();
+    }
+
+    private int getTranslation(RecyclerView.ViewHolder holder) {
+        DisplayMetrics dm = new DisplayMetrics();
+        ((WindowManager) holder.itemView.getContext()
+                .getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels - holder.itemView.getTop() - 2 * holder.itemView.getHeight();
     }
 }
