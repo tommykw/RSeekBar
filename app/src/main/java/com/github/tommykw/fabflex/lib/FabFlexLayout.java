12cc5e1dee6e687d3b5d79c42aa7a867628286a7
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-04-10 11:25:07 +0900

    Add fabstyle

--- app/src/main/java/com/github/tommykw/fabflex/lib/FabFlexLayout.java
+++ app/src/main/java/com/github/tommykw/fabflex/lib/FabFlexLayout.java
@@ -1,7 +1,45 @@
 package com.github.tommykw.fabflex.lib;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
 /**
  * Created by tommy on 2016/04/09.
  */
-public class FabFlexLayout {
+public class FabFlexLayout extends CoordinatorLayout implements View.OnTouchListener {
+    private FabFlexContainer fabFlexContainer;
+    private ItemAdapter.OnFabClickListener listener;
+
+    public FabFlexLayout(Context context) {
+        super(context);
+    }
+
+    public FabFlexLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FabFlexLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public FabFlexContainer getFabFlexContainer() {
+        return fabFlexContainer;
+    }
+
+    public interface OnContainerClickListener {
+        void onClick();
+    }
+
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        boolean tapped = true;
+        //if (tapped) listener.onClick();
+        return tapped;
+    }
 }
