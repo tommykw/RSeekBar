abed5dbff75ad1e50688f16c7d47bc1f3ddc4bbc
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-04-12 07:54:02 +0900

    [WIP] implements container

--- app/src/main/java/com/github/tommykw/fabflex/lib/FabFlexContainer.java
+++ app/src/main/java/com/github/tommykw/fabflex/lib/FabFlexContainer.java
@@ -2,6 +2,8 @@ package com.github.tommykw.fabflex.lib;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
@@ -10,7 +12,11 @@ import android.view.MotionEvent;
 /**
  * Created by tommy on 2016/04/09.
  */
+@CoordinatorLayout.DefaultBehavior(FabFlexContainer.FloatingLayoutBehavior.class)
 public class FabFlexContainer extends RecyclerView implements GestureDetector.OnGestureListener {
+    private GestureDetectorCompat gestureDetector;
+    private boolean isAdded = false;
+
     public FabFlexContainer(Context context) {
         super(context);
     }
@@ -21,11 +27,16 @@ public class FabFlexContainer extends RecyclerView implements GestureDetector.On
 
     public FabFlexContainer(Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        gestureDetector =new GestureDetectorCompat(context, this);
+    }
+
+    public void setFabFlexesAdded(boolean added) {
+        isAdded = added;
     }
 
     @Override
     public boolean onDown(MotionEvent e) {
-        return false;
+        return isAdded;
     }
 
     @Override
@@ -35,7 +46,7 @@ public class FabFlexContainer extends RecyclerView implements GestureDetector.On
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
-        return false;
+        return true;
     }
 
     @Override
@@ -51,5 +62,9 @@ public class FabFlexContainer extends RecyclerView implements GestureDetector.On
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         return false;
+    }
+
+    public static class FloatingLayoutBehavior
+            extends  CoordinatorLayout.Behavior<FabFlexContainer> {
     }
 }
